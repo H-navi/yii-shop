19995ab81109000f7613d5586a91f39f10ab27b1
h-navi <hanafinaufal@protonmail.com>
2021-05-19 10:32:39 +0700

    table & models

---
+++ console/migrations/m210518_032788_create_cart_items_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%cart_items}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%user}}`
+ */
+class m210518_032788_create_cart_items_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%cart_items}}', [
+            'id' => $this->primaryKey(),
+            'product_id' => $this->integer(11)->notNull(),
+            'order_id' => $this->integer(11)->notNull(),
+            'quantity' => $this->integer(2)->notNull(),
+            'created_by' => $this->integer(11),
+        ]);
+
+        // creates index for column `user_id`
+        $this->createIndex(
+            '{{%idx-cart_items-created_by}}',
+            '{{%cart_items}}',
+            'created_by'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-cart_items-created_by}}',
+            '{{%cart_items}}',
+            'created_by',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            '{{%fk-cart_items-product_id}}',
+            '{{%cart_items}}'
+        );
+
+        // drops index for column `user_id`
+        $this->dropIndex(
+            '{{%idx-cart_items-user_id}}',
+            '{{%cart_items}}'
+        );
+
+        $this->dropTable('{{%cart_items}}');
+    }
+}
